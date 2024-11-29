d1abc8634b381daa1672e1e13fc5a69d6dacb3a8
nayankadamm <nayankadam391@gmail.com>
2024-11-29 20:38:07 +0530

    mongodb connected

--- server/db/config.js
+++ server/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/NoteDB")
+mongoose.connect("mongodb+srv://nayan:RfjedPVAP6n7B0p3@notescluster.iotmp.mongodb.net/NoteDB")
